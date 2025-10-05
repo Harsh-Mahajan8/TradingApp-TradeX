94a91823c610a9c996a72b76388f5ab44a7a03ce
Harsh-Mahajan8 <harshmahajansk@gmail.com>
2025-10-05 22:34:44 +0530

    updating frontendURL

--- backend/app.js
+++ backend/app.js
@@ -23,9 +23,10 @@ app.listen(PORT, () => {
 })
 
 app.use(cors({
-  origin: ["https://tradingapp-tradex-nmgi.onrender.com/", "http://localhost:5173"], // allow only your frontend
-  credentials: true, credentials: true, // important since you are using withCredentials
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+  origin: ["https://tradingapp-tradex-nmgi.onrender.com", "http://localhost:5173"], 
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(bodyParser.json());
