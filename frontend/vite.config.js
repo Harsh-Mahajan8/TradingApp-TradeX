4dea3abf931d58202b795b5b8db3e5f2b74a550b
Harsh-Mahajan8 <harshmahajansk@gmail.com>
2025-10-06 19:56:41 +0530

    adding base"/" in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
   test: {
     globals: true,       // allows using describe/it/expect without imports
     environment: 'jsdom', // simulates browser DOM for component tests
